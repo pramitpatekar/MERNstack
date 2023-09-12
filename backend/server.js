1e59fd2f76acb4f060552997402a347339cf48d6
pramitpatekar <77798733+pramitpatekar@users.noreply.github.com>
2023-09-12 12:49:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,16 @@ app.use((req, res, next) => {
     next()
 })
 
+
 app.get('/', (req,res) => {
     res.json({mssg:'Welcome to the App'})
 })
 
 app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
+})
+
+app.listen2(process.env.PORT, () => {
     console.log('listening on port', process.env.PORT)
 })
 
