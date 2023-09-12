6248898819109cece50c2fdf1aaf39d2b0097523
pramitpatekar <77798733+pramitpatekar@users.noreply.github.com>
2023-09-12 20:41:41 +0530

    Second version

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ require('dotenv').config()
 
 const express = require('express')
 
+const workoutRoutes = require('./routes/workouts')
+
 const app = express()
+
+app.use(express.json())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
@@ -10,13 +14,17 @@ app.use((req, res, next) => {
 })
 
 
-app.get('/', (req,res) => {
-    res.json({mssg:'Welcome to the App'})
-})
+//  app.get('/', (req,res) => {
+//  res.json({mssg:'Welcome to the App'})
+//  })
+
+app.use('/api/workouts',workoutRoutes)
+
 
 app.listen(process.env.PORT, () => {
     console.log('listening on port', process.env.PORT)
 })
+
 
 
 
