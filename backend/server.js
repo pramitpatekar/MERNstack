1be7076767a2f6b1d52cd07475ca75191c541f57
pramitpatekar <77798733+pramitpatekar@users.noreply.github.com>
2023-09-13 15:27:47 +0530

    Third version

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
-
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 const app = express()
@@ -20,10 +20,19 @@ app.use((req, res, next) => {
 
 app.use('/api/workouts',workoutRoutes)
 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db and listening on port', process.env.PORT)
+        })
+    })
 
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+
+    .catch((error) => {
+        console.log(error)
+    })
+
+
 
 
 
