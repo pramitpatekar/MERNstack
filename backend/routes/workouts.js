c4ae5bd93fd92393aef2973ffb9e9d22e848d94c
pramitpatekar <77798733+pramitpatekar@users.noreply.github.com>
2023-09-13 22:28:01 +0530

    Fifth version

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,32 +1,22 @@
 const express = require('express')
-const Workout = require('../models/workoutModel')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout
+
+} = require('../controllers/workoutController')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all workouts'})
-})
+router.get('/', getWorkouts)
 
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'GET a single workout'})
-})
 
-router.post('/', async (req, res) => {
-    const {title, load, reps} = req.body
+router.get('/:id', getWorkout)
 
-    try
-    {
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    }
-    catch(error)
-    {
-        res.status(400).json({error: error.message})
-    }
 
-//    res.json({mssg: 'POST a new workout'})
+router.post('/', createWorkout)
 
-})
+
 
 router.delete('/:id', (req, res) => {
     res.json({mssg: 'DELETE a new workout'})
