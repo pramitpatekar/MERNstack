4703f3bd416e536651b5dd90b790f064bd3323d8
pramitpatekar <77798733+pramitpatekar@users.noreply.github.com>
2023-09-21 00:21:01 +0530

    Auth - 2nd version

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,17 @@ const loginUser = async (req, res) => {
 
 //signup user
 const signupUser = async (req, res) => {
-    res.json({mssg: 'Signup User'})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.signup(email, password)
+        
+        res.status(200).json({email, user})
+    }
+    catch(error) {
+        res.status(400).json({error: error.message})
+    }
+
 }
 
 
