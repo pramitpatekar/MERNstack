4d7eb9df62b02632e4e7fc4d97b46fb09f7892e7
pramitpatekar <77798733+pramitpatekar@users.noreply.github.com>
2023-09-24 23:54:06 +0530

    Auth - 7th version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+
 function App() {
   return (
     <div className="App">
@@ -14,6 +18,17 @@ function App() {
               path="/"
               element={<Home />}
             />
+
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+
+            <Route
+              path="/signup"
+              element={<Signup />}
+            />
+
           </Routes>
         </div>
 
