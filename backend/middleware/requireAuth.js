a4d7024fe95cc278ebfe0d7a5a45125a80ce532f
pramitpatekar <77798733+pramitpatekar@users.noreply.github.com>
2023-09-26 23:17:26 +0530

    Auth - 13th version

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -6,7 +6,7 @@ const requireAuth = async (req, res, next) => {
     // verify authentication
     const { authorization } = req.headers
 
- 
+
     if(!authorization) {
         return res.status(401).json({error: 'Authorization token required'})
     }
